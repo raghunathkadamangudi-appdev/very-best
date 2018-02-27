91e277eeea9c7d5cadfe3d79d300c35956857ccb
Josef Omar <jomar@uchicago.edu>
2018-02-27 22:18:13 +0000

    updated edits tuesday

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,7 +2,8 @@ class DishesController < ApplicationController
   def index
     @q = Dish.ransack(params[:q])
     @dishes = @q.result(:distinct => true).includes(:cuisine, :bookmarks, :fans, :specialists).page(params[:page]).per(10)
-
+    
+    
     render("dishes/index.html.erb")
   end
 
